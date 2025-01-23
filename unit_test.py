0cc2d1fac61a8916ebd9707052dd02b4b6d003d7
Adenm2024 <aden2023@uw.edu>
2025-01-22 23:14:11 -0800

    Update unit_test.py
    Added comments

--- unit_test.py
+++ unit_test.py
@@ -1,24 +1,24 @@
-from team_mines import MineSweeper
+from team_one_mines import MineSweeper
 # Changed variable and method to public for unit test 
 import unittest
 
-
+# This class tests the team_one_mines.py
 class TestMineSweeper(unittest.TestCase):
 
-    def setUp(self):
+    def setUp(self): # Tests minefield input
         self.ms = MineSweeper("minefield.txt")
 
-    def test__read_minefield(self):
+    def test__read_minefield(self): # Tests to read a file and generate right content
         self.assertEqual(self.ms.files , ['. . . .\n. . . .\n* . . .\n. . * .\n. . . .'])
 
-    def test_generate_hint(self):
+    def test_generate_hint(self): # Tests the generator hint method
         self.assertEqual(self.ms.generate_hint(2, 1), 1)
 
 
-    def test_generate_map(self):
+    def test_generate_map(self): # Tests the generated map 
         self.assertEqual(self.ms.fields, ['Field #1:\n0000000\n1100000\n*101110\n1101*10\n0001110\n'])
 
-    def test_create_output(self):
+    def test_create_output(self): # Tests the created output file and right content
         with open('minesweeper_output.txt', 'r') as f:
             contents = f.read()
 
@@ -26,6 +26,6 @@ class TestMineSweeper(unittest.TestCase):
 
 
 
-if __name__ =="__main__":
+if __name__ =="__main__": # It is a unti test funtion
     unittest.main()
 
